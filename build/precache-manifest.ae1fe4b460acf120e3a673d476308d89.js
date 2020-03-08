ea80b9c70e7d9f029bcf99546898e6db592e5037
k_maryam27 <k_maryam27@yahoo.com>
2020-03-08 01:52:45 -0500

    add redux react-redux lib

---
+++ build/precache-manifest.ae1fe4b460acf120e3a673d476308d89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "988634958cbbf4e6bb71da8497f5213a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "690dda0c8323534bc4ca",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "eabdedd40a50a7dcbea8",
+    "url": "/static/js/2.be3bc254.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.be3bc254.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "690dda0c8323534bc4ca",
+    "url": "/static/js/main.34379ad4.chunk.js"
+  },
+  {
+    "revision": "978ed8c05c3e0c34c3d9",
+    "url": "/static/js/runtime-main.53940367.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
